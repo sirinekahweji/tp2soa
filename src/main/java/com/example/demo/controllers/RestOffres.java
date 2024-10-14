c4e210f8c16dcd4cb425574f0bc70c48bf29f59e
sirine kahweji <sirinekahweji5@gmail.com>
2024-10-14 16:38:23 +0200

    add controller

--- src/main/java/com/example/demo/controllers/RestOffres.java
+++ src/main/java/com/example/demo/controllers/RestOffres.java
@@ -1,4 +1,40 @@
 package com.example.demo.controllers;
 
+
+import com.example.demo.Entities.Offre;
+import com.example.demo.Repositories.OffreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Offres")
 public class RestOffres {
+
+    @Autowired
+    OffreRepository offreRepository;
+
+    @GetMapping
+    public List<Offre> getAll() {
+        return offreRepository.findAll();
+    }
+
+    @GetMapping("/{uid}")
+    public Offre getbyid(@PathVariable Long uid ) {
+
+        return offreRepository.findById(uid).get();
+    }
+
+    @PostMapping
+    public Offre saveoffre(@RequestBody Offre newoffre) {
+        return offreRepository.save(newoffre);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteoffre(@PathVariable Long id) {
+        offreRepository.deleteById(id);
+    }
 }
+
+
